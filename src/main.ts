8cc8b41fa53fdc6f409e36eceef49df7fcc57112
Simon Schmidt <mail@sjs.one>
2022-09-20 00:25:10 +0200

    Moved files to different package

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,5 @@
 import NodeFs from 'fs'
-import { ControllerContext } from './core/controllerContext'
-
-import { Lexer } from "./core/lexer"
 import { ObjectTreeParser } from "./core/objectTreeParser/objectTreeParser"
-import { Parser } from './core/parser'
-import { Runtime } from './core/runtime'
-import { Token } from "./core/token"
-import { CaseImplementation } from './fusionObjects/caseImplementation'
-import { ComponentImplementation } from './fusionObjects/componentImplementation'
-import { FusionObjectManager } from './fusionObjects/fusionObjectManager'
-import { MatcherImplementation } from './fusionObjects/matcherImplementation'
-import { FusionView } from './fusionView'
-
-
-
-
-// const consoleLog = console.log
-// console.log = (...messages) => {
-//     const mainMessage = messages.shift()
-//     consoleLog((messages.length > 0 ? `[${mainMessage}]` : mainMessage))
-//     for(const message of messages) {
-//         consoleLog(message)
-//     }
-//     if(messages.length > 0) {
-//         consoleLog("\n")
-//     }
-// }
-
-
 
 const fusion = `
 prototype(Neos.Fusion:Component).@class = "Neos\\Fusion\\FusionObjects\\ComponentImplementation"
@@ -41,20 +13,12 @@ prototype(Test.Tset:Component) {
     @if.test = \${props.test}
 }
 `
-FusionObjectManager.set('Neos\\Fusion\\FusionObjects\\ComponentImplementation', ComponentImplementation)
-FusionObjectManager.set('Neos\\Fusion\\FusionObjects\\CaseImplementation', CaseImplementation)
-FusionObjectManager.set('Neos\\Fusion\\FusionObjects\\MatcherImplementation', MatcherImplementation)
 
+// const fusionFile = NodeFs.readFileSync('./test.fusion').toString()
 
-const fusionFile = NodeFs.readFileSync('./test.fusion').toString()
-
-// const objectTreeParser = ObjectTreeParser.parse(fusion)
-// //console.log(JSON.stringify(objectTreeParser.statementList.statements, undefined, 4))
-// objectTreeParser.statementList.debugPrint('', false)
+const objectTreeParser = ObjectTreeParser.parse(fusion)
+//console.log(JSON.stringify(objectTreeParser.statementList.statements, undefined, 4))
+objectTreeParser.statementList.debugPrint('', false)
 
 // const fusionAst = (new Parser()).parse(fusion);
 // console.log("fusionAst", JSON.stringify(fusionAst.__prototypes, undefined, 4))
-
-const fusionView = new FusionView
-
-console.log(fusionView.render())

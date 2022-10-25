365b9fb925a34d50cb9f7729f8bf5182d8be460d
Simon Schmidt <mail@sjs.one>
2022-10-25 22:23:23 +0200

    Added missing parent to value of TagAttributeNode

--- src/afx/nodes/TagAttributeNode.ts
+++ src/afx/nodes/TagAttributeNode.ts
@@ -4,11 +4,16 @@ import { NodePosition } from "./NodePosition";
 export class TagAttributeNode extends AbstractNode {
     public name: string
     public value: any
-    
+
     constructor(position: NodePosition, name: string, value: any) {
         super(position)
         this.name = name
         this.value = value
-        if(this.value instanceof AbstractNode) this.value["parent"] = this
+        if (Array.isArray(this.value)) {
+            for (const element of this.value) {
+                if (element instanceof AbstractNode) element["parent"] = this
+            }
+        }
+
     }
 }

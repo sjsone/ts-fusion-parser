bf285d7ca97d42f4e04b74ccb51686749593b73a
Simon Schmidt <mail@sjs.one>
2023-02-02 23:14:27 +0100

    chore: remove quotes from LiteralStringNode Value

--- src/dsl/eel/nodes/LiteralStringNode.ts
+++ src/dsl/eel/nodes/LiteralStringNode.ts
@@ -1,5 +1,14 @@
+import { AbstractNode } from "../../../common/AbstractNode";
+import { NodePositionInterface } from "../../../common/NodePositionInterface";
 import { AbstractLiteralNode } from "./AbstractLiteralNode";
 
-export class LiteralStringNode extends AbstractLiteralNode<string> {    
+export class LiteralStringNode extends AbstractLiteralNode<string> {
+    protected quotationType: string
 
+    constructor(value: string, position: NodePositionInterface, parent: AbstractNode | undefined = undefined) {
+        const quotationType = value[0]
+        value = value.substring(1, value.length - 1);
+        super(value, position, parent)
+        this.quotationType = quotationType
+    }
 }

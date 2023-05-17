1dd6edf85658249ca81431ae403006e6ca218eaf
Simon Schmidt <mail@sjs.one>
2023-05-18 01:00:02 +0200

    todo: check if `nodes` really has to be an array -> per `eelExpressionValue.nodes = eelNodes` probably not

--- src/fusion/nodes/EelExpressionValue.ts
+++ src/fusion/nodes/EelExpressionValue.ts
@@ -5,7 +5,7 @@ import { NodePosition, NodePositionStub } from "../../common/NodePosition";
 
 export class EelExpressionValue extends AbstractPathValue {
     public value = ""
-    // TODO: check if `nodes` really has to be an array
+    // TODO: check if `nodes` really has to be an array -> per `eelExpressionValue.nodes = eelNodes` probably not
     public nodes: AbstractNode[] = []
 
     public constructor() {
